c1f65d12534dcab1d150519fbf846939696c51dd
MohammadMahdiOmid <mohammadmehdiomid@gmail.com>
2021-08-01 10:54:26 +0430

    load image is added

---
+++ input_image/landsat.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage import io
+
+# load images
+blue = io.imread("E:/Company/KAVOSHGARAN/Virtual Irrigation/Image/Landsat/Blue.tif")
+green = io.imread("E:/Company/KAVOSHGARAN/Virtual Irrigation/Image/Landsat/Green.tif")
+red = io.imread("E:/Company/KAVOSHGARAN/Virtual Irrigation/Image/Landsat/Red.tif")
+nir = io.imread("E:/Company/KAVOSHGARAN/Virtual Irrigation/Image/Landsat/NIR.tif")
+
+# shape of images
+print(blue.shape)
+print(green.shape)
+print(red.shape)
+print(nir.shape)
+
+# change to array
+b_arr = np.array(blue).astype(float)
+g_arr = np.array(green).astype(float)
+r_arr = np.array(red).astype(float)
+n_arr = np.array(nir).astype(float)
+# print(b_arr,g_arr,r_arr,n_arr)
+# print(b_arr[100][25])
+
+
+# dimention
+print(b_arr.ndim)
+
+# type
+print(g_arr.dtype)
+
+# size
+print(r_arr.size)
+
+# itemsize
+print(n_arr.itemsize)
